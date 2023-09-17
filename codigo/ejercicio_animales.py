9458a996e17502069ae25c561886f2e53703f2b6
Mariano Andolina <mariano.3gs@gmail.com>
2023-09-17 12:24:20 -0300

    ejercicio zoo

--- codigo/ejercicio_animales.py
+++ codigo/ejercicio_animales.py
@@ -7,11 +7,29 @@ import os
 print(os.getcwd())
 
 # realizamos la lectura con Pandas del archivo csv en la variable salida que luego nos permite utilizar los metodos de pandas
-salida = pd.read_csv(r'C:\Users\maria\Desktop\Programacion\python\datos-edx\zoo.csv', sep=';' )
+salida = pd.read_csv(r'C:\Users\maria\Desktop\Programacion\python\datos-edx\codigo\zoo.csv', sep=',' )
 
 # realizamos la coneccion a la base de datos, recordamos que si no existe la db la va a crear
 con = sqlite3.connect(r'C:\Users\maria\Desktop\Programacion\python\datos-edx\db\zoo.db')
 
 # el metodo to_sql() es un metodo de pandas que nos permite
 salida.to_sql('zoo', con, if_exists='replace', index=False)
+
+# creamos el objeto cursor para ejecutar sentencias SQLite
+cursor = con.cursor()
+
+"""
+EN ESTE PUNTO REALIZAMOS LA EJECUCION DEL CODIGO HASTA ACA
+
+* Lectura de archivo csv
+* Creacion de base de datos y su conexion
+* Paso de datos del csv a la base de datos con el metodo to_sql() de pandas
+* Creacion de objeto cursor para ejecutar sentencias SQL
+"""
+
+consulta1= "SELECT name FROM pragma_table_info('zoo')"
+
+cursor.execute(consulta1)
+filas = cursor.fetchall()
+print(filas)
 
