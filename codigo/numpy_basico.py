a97e2c371ae52b1276f9718ac14fa052c0c37214
Mariano Andolina <mariano.3gs@gmail.com>
2023-09-17 22:21:42 -0300

    plotly, numpy, dash inicio

---
+++ codigo/numpy_basico.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# creamos una lista en python que luego convertiremos en numpy array
+mi_lista = [1,2,3,4,]
+
+# creamos el array de numpy mediante la lista creada con python
+mi_array = np.array(mi_lista)
+
+# mostramos la lista de python y el tipo
+print(mi_lista)
+print(type(mi_lista))
+
+# mostramos el array de np y el tipo
+print(mi_array)
+print(type(mi_array))
+
+
+
